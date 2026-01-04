f5ea08b2ec11a3829d97b25393a819be5d5048b7
Anton Smolyaninov <antonsmol77@gmail.com>
2026-01-05 02:16:27 +0800

    add ststic and chang catalog/templates/contacts.html, catalog/templates/home.html, catalog/urls.py, catalog/views.py, config/settings.py, config/urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,11 @@
+from catalog import views
 from catalog.apps import CatalogConfig
 from django.urls import path
-from catalog.views import home
+
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("", home)
+    path('home/', views.home, name='home'),
+    path('contacts/', views.contacts, name='contacts'),
 ]
--- config/settings.py
+++ config/settings.py
@@ -83,8 +83,8 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
-STATTICFILES_DIRS = [BASE_DIR / "static",]
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 DEFAUTLT_AUOTO_FILED = "django.db.models.BigAuotoField"
