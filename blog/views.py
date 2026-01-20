ac92dada7cfd36ca0d71b71ccc3206a55d08ea12
Anton Smolyaninov <antonsmol77@gmail.com>
2026-01-20 23:57:33 +0800

    add blog/urls.py

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy, reverse
+from .models import BlogPost
 
-# Create your views here.
+class BlogPostListView(ListView):
+    model = BlogPost
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    queryset = BlogPost.objects.filter(is_published=True).order_by('-created_at')
+
+class BlogPostDetailView(DetailView):
+    model = BlogPost
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+
+    def get(self, request, *args, **kwargs):
+        # Увеличиваем счетчик просмотров
+        response = super().get(request, *args, **kwargs)
+        self.object.views += 1
+        self.object.save(update_fields=['views'])
+        return response
+
+class BlogPostCreateView(CreateView):
+    model = BlogPost
+    fields = ['title', 'content', 'preview', 'is_published']
+    template_name = 'blog/post_form.html'
+    success_url = reverse_lazy('blog:post_list')
+
+class BlogPostUpdateView(UpdateView):
+    model = BlogPost
+    fields = ['title', 'content', 'preview', 'is_published']
+    template_name = 'blog/post_form.html'
+
+    def get_success_url(self):
+
+        return reverse('blog:post_detail', kwargs={'pk': self.object.pk})
+
+class BlogPostDeleteView(DeleteView):
+    model = BlogPost
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('blog:post_list')
