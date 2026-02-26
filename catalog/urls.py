89ac127130dff0b7c68a5e94ad18b925185be3ae
Anton Smolyaninov <antonsmol77@gmail.com>
2026-02-26 23:05:56 +1100

    add redis and cache

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
+
 from catalog.views import (
     HomePageView,
     ContactPageView,
@@ -20,7 +22,7 @@ urlpatterns = [
     path('product/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
     path('product/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
 
-    path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('product/<int:pk>/', cache_page(60)(ProductDetailView.as_view()), name='product_detail'),
 
     path('product/', ProductListView.as_view(), name='product_list'),
 ]
