0d2ccb907a985f338522aa358966b4798d2f7615
Anton <antonsmol77@gmail.com>
2026-03-01 20:00:15 +1100

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -109,8 +109,8 @@ LOGOUT_REDIRECT_URL = "/"
 
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
-EMAIL_HOST_USER = "anton1985smolyaninov@yandex.ru"
-EMAIL_HOST_PASSWORD = "exvcyclkvvvoetid"
+EMAIL_HOST_USER = "@yandex.ru"
+EMAIL_HOST_PASSWORD = ""
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
 
